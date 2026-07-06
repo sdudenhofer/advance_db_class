7f2f9c70f1a8f50cbb0d58fdd7e3f5bc44d00913
Seth Dudenhofer <sdudenhofer@gmail.com>
2026-07-06 13:56:45 -0500

    Delete create_detail_summary.sql
    removed redundant file

--- create_detail_summary.sql
+++
@@ -1,5 +0,0 @@
-SELECT 
-film_title, city, country, STRING_AGG(actor_firstname || ' ' || actor_lastname, ', ') as actors, COUNT(rental_id) as times_rented, 
-rental_rate, (rental_rate * COUNT(rental_id)) as total_paid, store_id
-FROM detail_summary
-GROUP BY film_title, city, country, store_id, rental_rate

138e368be7b46a0562368959a19c5d1f9e85d2b4
Seth Dudenhofer <seth@sent.as>
2026-07-06 14:02:57 -0500

    create summary table

---
+++ create_summary_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE summary_table (
+	film_title VARCHAR(50),
+	city VARCHAR(50),
+	country VARCHAR(50),
+	actors VARCHAR(200),
+	total_paid DECIMAL(7,2),
+	times_rented SMALLINT,
+	store_id SMALLINT
+)
+

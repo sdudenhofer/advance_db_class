85674e6f82cd2cbade3bb1d0f26445fdfda13bd4
Seth Dudenhofer <seth@sent.as>
2026-07-06 13:54:04 -0500

    create detail table

---
+++ create_detail_table.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE detail_table (
+	city varchar(50),
+	country varchar(50),
+	store_id SMALLINT,
+	amount DECIMAL(5,2),
+	rental_rate DECIMAL(5,2),
+	actor_firstname varchar(50),
+	actor_lastname varchar(50),
+	film_title varchar(50),
+	film_id SMALLINT,
+	rental_date TIMESTAMP,
+	rental_id smallint,
+)

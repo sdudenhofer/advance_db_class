b835d15b4decd2d4504a44b61e578a74a804afb9
Seth Dudenhofer <seth@sent.as>
2026-07-10 11:19:28 -0500

    change total_paid amount logic

--- add_data_to_summary_table.sql
+++ add_data_to_summary_table.sql
@@ -1,5 +1,6 @@
 INSERT INTO summary_table (film_title, city, country, actors, total_paid, times_rented, store_id)
-SELECT 
-film_title, city, country, actor_firstname || ' ' || actor_lastname, COUNT(rental_id) as times_rented, store_id
+SELECT
+    film_title, city, country, actor_firstname || ' ' || actor_lastname,
+    SUM(amount * COUNT(rental_id)), COUNT(rental_id) as times_rented, store_id
 FROM detail_summary
 GROUP BY film_title, city, country, store_id

fe88b46011e97424e243f00311f5159f91073785
Seth Dudenhofer <seth@sent.as>
2026-07-13 11:30:40 -0500

    changed rows needed in summary table

--- create_summary_table.sql
+++ create_summary_table.sql
@@ -1,10 +1,6 @@
 CREATE TABLE summary_table (
-	film_title VARCHAR(50),
-	city VARCHAR(50),
-	country VARCHAR(50),
-	actors VARCHAR(200),
-	total_paid DECIMAL(7,2),
-	times_rented SMALLINT,
-	store_id SMALLINT
-)
-
+    city VARCHAR(50),
+    country VARCHAR(50),
+    total_sales DECIMAL(7, 2),
+    date_updated TIMESTAMP
+);

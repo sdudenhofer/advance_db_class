84f8c0a14c6bb032d7540ecfd810148423d1e53b
Seth Dudenhofer <sdudenhofer@gmail.com>
2026-07-23 10:04:33 -0500

    Refactor detail_summary INSERT statement
    Refactor INSERT statement for detail_summary to improve readability and fix column references.

--- advanced_db_performance_assessment.sql
+++ advanced_db_performance_assessment.sql
@@ -23,19 +23,32 @@ date_generated TIMESTAMPTZ
 );
 
 --ADD DATA TO DETAIL TABLE
-INSERT INTO detail_summary (location, store_id, amount, rental_rate, difference, actor_firstname, actor_lastname, film_title,
-film_id, rental_date, rental_id)
-SELECT city.city || ‘, ‘ || country.country as location, inventory.store_id, payment.amount, film.rental_amount, payment.amount – film.rental_amount AS difference, actor.firstname, actor.lastname, film.title, inventory.film_id, rental.rental_date, rental.rental_id
+INSERT INTO detail_summary (
+    location, store_id, amount, rental_rate, difference,
+    actor_firstname, actor_lastname, film_title, film_id, rental_date, rental_id
+)
+SELECT
+    city.city || ', ' || country.country AS location,
+    inventory.store_id,
+    payment.amount,
+    film.rental_rate,
+    payment.amount - film.rental_rate AS difference,
+    actor.first_name,
+    actor.last_name,
+    film.title,
+    inventory.film_id,
+    rental.rental_date::DATE,
+    rental.rental_id
 FROM rental
-JOIN inventory on rental.inventory_id = inventory.inventory_id
-JOIN store on inventory.store_id = store.store_id
-JOIN address on store.address_id = address_id
-JOIN city on address.city_id = city.city_id
-JOIN country on city.country_id = country.country_id
-JOIN payment on rental.rental_id = payment.rental_id
-JOIN film on inventory.film_id = film.film_id
-JOIN film_actor on film.film_id = film_actor.film_id
-JOIN actor on film_actor.actor_id = actor.actor_id;
+JOIN inventory ON rental.inventory_id = inventory.inventory_id
+JOIN store ON inventory.store_id = store.store_id
+JOIN address ON store.address_id = address.address_id
+JOIN city ON address.city_id = city.city_id
+JOIN country ON city.country_id = country.country_id
+JOIN payment ON rental.rental_id = payment.rental_id
+JOIN film ON inventory.film_id = film.film_id
+JOIN film_actor ON film.film_id = film_actor.film_id
+JOIN actor ON film_actor.actor_id = actor.actor_id;
 
 -- CREATE TRIGGER THAT WILL UPDATE SUMMARY TABLE
 CREATE OR REPLACE FUNCTION update_summary_table_after_insert()

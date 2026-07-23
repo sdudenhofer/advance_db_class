f8aa7d8e83edb44f54a7b762cbf1caeb8eb1bf2b
Seth Dudenhofer <sdudenhofer@gmail.com>
2026-07-23 10:48:14 -0500

    Refactor summary_table and detail_summary insertions
    Updated data types for total_amount, total_rental_rate, and total_difference in summary_table. Modified INSERT statements to ensure correct data insertion and updated column references.

--- advanced_db_performance_assessment.sql
+++ advanced_db_performance_assessment.sql
@@ -16,9 +16,9 @@ rental_id SMALLINT
 --CREATE summary_table
 CREATE TABLE summary_table(
 location VARCHAR(150),
-total_amount DECIMAL(7,2),
-total_rental_rate DECIMAL(7,2),
-total_difference DECIMAL(7,2),
+total_amount DECIMAL(10,2),
+total_rental_rate DECIMAL(10,2),
+total_difference DECIMAL(10,2),
 date_generated TIMESTAMPTZ
 );
 
@@ -56,9 +56,8 @@ RETURNS TRIGGER AS $$
 BEGIN
 INSERT INTO summary_table(location, total_amount,
 total_rental_rate, total_difference, date_generated)
-SELECT title, location, SUM(amount), SUM(rental_rate), SUM(difference),
-NOW()
-FROM detail_summary
+SELECT location, SUM(amount), SUM(rental_rate), SUM(difference), NOW()
+  FROM detail_summary
 GROUP BY location;
 RETURN NEW;
 END;
@@ -74,15 +73,15 @@ LANGUAGE plpgsql as $$
 BEGIN
 DELETE FROM detail_summary;
 DELETE FROM summary_table;
-INSERT INTO detail_summary (city, country, store_id, amount, rental_rate, actor_firstname, actor_lastname,
-film_title, film_id, rental_date, rental_id)
-SELECT city.city || ', ' || country.country as location, inventory.store_id, payment.amount, film.rental_amount, 
-payment.amount - film.rental_amount AS difference, actor.firstname, actor.lastname, film.title, inventory.film_id, 
+INSERT INTO detail_summary (location, store_id, amount, rental_rate, difference,
+    actor_firstname, actor_lastname, film_title, film_id, rental_date, rental_id)
+SELECT city.city || ', ' || country.country as location, inventory.store_id, payment.amount, film.rental_rate, 
+payment.amount - film.rental_rate AS difference, actor.first_name, actor.last_name, film.title, inventory.film_id, 
 rental.rental_date, rental.rental_id
 FROM rental
 JOIN inventory on rental.inventory_id = inventory.inventory_id
 JOIN store on inventory.store_id = store.store_id
-JOIN address on store.address_id = address_id
+JOIN address on store.address_id = address.address_id
 JOIN city on address.city_id = city.city_id
 JOIN country on city.country_id = country.country_id
 JOIN payment on rental.rental_id = payment.rental_id
@@ -90,6 +89,3 @@ JOIN film on inventory.film_id = film.film_id
 JOIN film_actor on film.film_id = film_actor.film_id
 JOIN actor on film_actor.actor_id = actor.actor_id;
 END;$$;
-
-
-
